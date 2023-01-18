c8534cb583ee01f3ef116d2491d6a438a5b2bab7
sarvar-bakhadirovic <sarvarxalimbetov60@gmail.com>
2023-01-18 17:39:59 +0500

    finished

--- script.js
+++ script.js
@@ -16,111 +16,41 @@ let _peopleInput = document.querySelector('.people-input')
 _btn1.addEventListener('change', e => {
     e.preventDefault()
     let tip = e.target.value
-    let result = Number(_billInput.value) + (Number(_billInput.value) * (tip / 100))
-
-    if(Number(_peopleInput.value) != 0){
-        _amount.textContent = `
-            $${countSum(Number(_billInput.value), tip, Number(_peopleInput.value))}
-        `
-
-        _total.textContent = `$${result.toFixed(2)}`
-    } else {
-        document.querySelector('.message').style.display = 'block'
-        document.querySelector('.people-input').style.border = '2px solid red'
-        setTimeout((e) => {
-            document.querySelector('.message').style.display = 'none'
-            document.querySelector('.people-input').style.border = '2px solid transparent'
-        }, 3000)
-    }
+    render(tip)
 })
 
 _btn2.addEventListener('change', e => {
     e.preventDefault()
     let tip = e.target.value
-    let result = Number(_billInput.value) + (Number(_billInput.value) * (tip / 100))
-
-    if(Number(_peopleInput.value) != 0){
-        _amount.textContent = `
-            $${countSum(Number(_billInput.value), tip, Number(_peopleInput.value))}
-        `
-
-        _total.textContent = `$${result.toFixed(2)}`
-    } else {
-        document.querySelector('.message').style.display = 'block'
-        document.querySelector('.people-input').style.border = '2px solid red'
-        setTimeout((e) => {
-            document.querySelector('.message').style.display = 'none'
-            document.querySelector('.people-input').style.border = '2px solid transparent'
-        }, 3000)
-    }
+    render(tip)
 })
 
 _btn3.addEventListener('change', e => {
     e.preventDefault()
     let tip = e.target.value
-    let result = Number(_billInput.value) + (Number(_billInput.value) * (tip / 100))
-
-    if(Number(_peopleInput.value) != 0){
-        _amount.textContent = `
-            $${countSum(Number(_billInput.value), tip, Number(_peopleInput.value))}
-        `
-
-        _total.textContent = `$${result.toFixed(2)}`
-    } else {
-        document.querySelector('.message').style.display = 'block'
-        document.querySelector('.people-input').style.border = '2px solid red'
-        setTimeout((e) => {
-            document.querySelector('.message').style.display = 'none'
-            document.querySelector('.people-input').style.border = '2px solid transparent'
-        }, 3000)
-    }
+    render(tip)
 })
 
 _btn4.addEventListener('change', e => {
     e.preventDefault()
     let tip = e.target.value
-    let result = Number(_billInput.value) + (Number(_billInput.value) * (tip / 100))
-
-    if(Number(_peopleInput.value) != 0){
-        _amount.textContent = `
-            $${countSum(Number(_billInput.value), tip, Number(_peopleInput.value))}
-        `
-
-        _total.textContent = `$${result.toFixed(2)}`
-    } else {
-        document.querySelector('.message').style.display = 'block'
-        document.querySelector('.people-input').style.border = '2px solid red'
-        setTimeout((e) => {
-            document.querySelector('.message').style.display = 'none'
-            document.querySelector('.people-input').style.border = '2px solid transparent'
-        }, 3000)
-    }
+    render(tip)
 })
 
 _btn5.addEventListener('change', e => {
     e.preventDefault()
     let tip = e.target.value
-    let result = Number(_billInput.value) + (Number(_billInput.value) * (tip / 100))
-
-    if(Number(_peopleInput.value) != 0){
-        _amount.textContent = `
-            $${countSum(Number(_billInput.value), tip, Number(_peopleInput.value))}
-        `
-
-        _total.textContent = `$${result.toFixed(2)}`
-    } else {
-        document.querySelector('.message').style.display = 'block'
-        document.querySelector('.people-input').style.border = '2px solid red'
-        setTimeout((e) => {
-            document.querySelector('.message').style.display = 'none'
-            document.querySelector('.people-input').style.border = '2px solid transparent'
-        }, 3000)
-    }
+    render(tip)
 })
 
 _btn6.addEventListener('change', e => {
     e.preventDefault()
     let tip = e.target.value
+    render(tip)
+})
+
+
+function render(tip){
     let result = Number(_billInput.value) + (Number(_billInput.value) * (tip / 100))
 
     if(Number(_peopleInput.value) != 0){
@@ -137,10 +67,7 @@ _btn6.addEventListener('change', e => {
             document.querySelector('.people-input').style.border = '2px solid transparent'
         }, 3000)
     }
-})
-
-
-
+}
 
 function countSum(bill, tip, value){
     let _bill = Number(bill)
